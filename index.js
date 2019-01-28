690cb4d5127cc0f6fec4e8e1b53b1ec1c1191528
Jordan Rivera <jordan.rivera34@gmail.com>
2019-01-28 05:47:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+destructivelyAppendKitten('Ralph');
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+destructivelyRemoveLastKitten()
+
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+destructivelyPrependKitten('Ralph');
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+destructivelyRemoveFirstKitten();
+
+function appendKitten(name){
+  const kittens2 = [...kittens, name];
+  return kittens2;
+}
+
+appendKitten('Broom');
+
+function removeLastKitten() {
+  const kittens3 = kittens.slice(0, kittens - 1);
+}
