6f79e3c1e6e6a19505ad6fb9533952ff515873f9
Jordan Rivera <jordan.rivera34@gmail.com>
2019-01-28 05:57:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function prependKitten(name) {
 prependKitten('Broom');
 
 function removeFirstKitten() {
-  const kittens3 = kittens.slice();
+  const kittens3 = kittens2.slice();
   return kittens3;
 }
 
